4967d16d9f872388f8e5db6edb41a2f603a479e1
Sripada Lohith <lohithsripada@gmail.com>
2026-08-21 17:03:48 +0530

    Add Google Tag Manager and Sitemap for SEO

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -102,9 +102,15 @@ function RootShell({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N6764G6H');` }} />
         <HeadContent />
       </head>
       <body>
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6764G6H" height="0" width="0" style={{ display: "none", visibility: "hidden" }}></iframe></noscript>
         {children}
         <Scripts />
       </body>
